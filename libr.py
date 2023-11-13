a0d5db6a1cd0256c48e36b07f53c00935c4b6c94
levblch <142447432+levblch@users.noreply.github.com>
2023-11-13 17:51:56 +0300

    функции и библиотеки

---
+++ libr.py
@@ -0,0 +1,12 @@
+# from math import *
+# result = sqrt(25)
+
+# # import math
+# # result = math.sqrt(25)
+# print(result)
+# # print(dir(math))
+
+from random import randint
+rand_num = randint(1, 100)
+print(rand_num)
+
